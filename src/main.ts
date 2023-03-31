b4e270120d43952c9ee43ccb6d37ea93a579f8d9
Pedro Luís Brilhadori <pedroluisbrilhadori@gmail.com>
2023-03-31 10:20:01 -0300

    feat: adiciona https ao projeto

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,19 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { config } from 'dotenv';
+import { readFileSync } from 'fs';
 
 config();
 
 async function bootstrap() {
-    const app = await NestFactory.create(AppModule);
+    const httpsOptions = {
+        key: readFileSync('./certificates/localhost-key.pem'),
+        cert: readFileSync('./certificates/localhost.pem'),
+    };
+
+    const app = await NestFactory.create(AppModule, { httpsOptions });
     app.useGlobalPipes(new ValidationPipe());
+    app.enableCors();
     await app.listen(3001);
 }
 bootstrap();
