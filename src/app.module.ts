2cf7f8213a38b629b939255889a6d78a2898b55f
PedroLuisBrilhadori <pedroluisbrilhadori@gmail.com>
2023-04-04 13:43:56 -0300

    feat: separa configurações de conexões em um arquivo

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,26 +2,13 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { PdvModule } from './modules/pdv.module';
+import { ormConfig } from './config';
 
 @Module({
     controllers: [],
     imports: [
         ConfigModule.forRoot(),
-        TypeOrmModule.forRoot({
-            name: 'default',
-            type: 'mssql',
-            host: process.env.DB_HOST,
-            username: process.env.DB_USER,
-            password: process.env.DB_PASSWORD,
-            port: Number(process.env.DB_PORT),
-            database: process.env.DB_DATABASE,
-            synchronize: true,
-            autoLoadEntities: true,
-            logging: false,
-            extra: {
-                trustServerCertificate: true,
-            },
-        }),
+        TypeOrmModule.forRoot(ormConfig),
         PdvModule,
     ],
     providers: [],
